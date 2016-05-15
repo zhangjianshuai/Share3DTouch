84f33be1c20827866e658df81718d81f0dedaefd
zhangjianshuai <zhangjianshuai>
2016-05-15 14:23:21 +0800

    Initial Commit

---
+++ Share3DTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share3DTouch
+//
+//  Created by jens on 16/5/15.
+//  Copyright © 2016年 张建帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Share3DTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Share3DTouch
+//
+//  Created by jens on 16/5/15.
+//  Copyright © 2016年 张建帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
